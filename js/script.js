50bd1e1de3025e937c9c7eb28fc305dc83c7ba67
wlzcool <346594796@qq.com>
2016-04-06 23:42:41 +0800

    the AI part that did not finish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var me = true;
+var over = false;
 //防止重复落子
 var chessBoard = [];
 for (var i = 0; i < 15; i++) {
@@ -7,6 +8,62 @@ for (var i = 0; i < 15; i++) {
 		chessBoard[i][j] = 0;
 	}
 }
+var wins = [];
+for (var i = 0; i < 15; i++) {
+	wins[i] = [];
+	for (var j = 0; j < 15; j++) {
+		wins[i][j] = [];
+	}
+}
+var count = 0;
+//横线
+for (var i = 0; i < 15; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[i][j + k][count] = true;
+		}
+		count++;
+	}
+}
+//竖线
+for (var i = 0; i < 15; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[j + k][i][count] = true;
+		}
+		count++;
+	}
+}
+//斜线
+for (var i = 0; i < 11; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[i + k][j + k][count] = true;
+		}
+		count++;
+	}
+}
+//反斜线
+for (var i = 0; i < 11; i++) {
+	for (var j = 14; j > 3; j--) {
+		for (var k = 0; k < 5; k++) {
+			wins[i + k][j - k][count] = true;
+		}
+		count++;
+	}
+}
+
+console.log(count);
+
+//赢法的统计数组
+var myWin = [];
+var computerWin = [];
+
+for (var i = 0; i < count; i++) {
+	myWin[i] = 0;
+	computerWin[i] = 0;
+}
+
 var chess = document.getElementById("chess");
 var context = chess.getContext("2d");
 context.strokeStyle = "#bfbfbf";
@@ -14,7 +71,7 @@ var logo = new Image();
 logo.src = "img/logo.jpg";
 logo.onload = function() {
 	context.drawImage(logo, 420, 420, 30, 30);
-	drawChessLine();	
+	drawChessLine();
 }
 var drawChessLine = function() {
 	for (var i = 0; i < 15; i++) {
@@ -44,18 +101,47 @@ var oneStep = function(i, j, me) {
 	context.fill();
 }
 chess.onclick = function(e) {
+	if (over) {
+		return;
+	}
 	var x = e.offsetX;
 	var y = e.offsetY;
 	var i = Math.floor(x / 30);
 	var j = Math.floor(y / 30);
 	if (chessBoard[i][j] == 0) {
+		oneStep(i, j, me);
 		if (me) {
 			chessBoard[i][j] = 1;
+			for (var k = 0; k < count; k++) {
+				if (wins[i][j][k]) {
+					//myWin这里是个数组，如果存在一个k，使得mywin[k]=5,说明第k种赢法被实现
+					myWin[k]++;
+					computerWin[k] = 6;
+					if (myWin[k] == 5) {
+						alert("You win!!");
+						over = true;
+					}
+				}
+			}
 		} else {
 			chessBoard[i][j] = 2;
+			for (var k = 0; k < count; k++) {
+				if (wins[i][j][k]) {
+					//myWin这里是个数组，如果存在一个k，使得mywin[k]=5,说明第k种赢法被实现
+					myWin[k] = 6;
+					computerWin[k]++;
+					if (computerWin[k] == 5) {
+						alert("You lose!!");
+						over = true;
+					}
+				}
+			}
 		}
-		oneStep(i, j, me);
 		me = !me;
+		//每下一次都和赢法数组做一次遍历。
+
+
+
+
 	}
-	
 }
